f3bd72ecf92adacb96e9c1d276450272ec3042a7
Sergey Sergeev <gurugray@yandex.ru>
2021-04-05 11:56:13 +0300

    minimize css

--- .eleventy.js
+++ .eleventy.js
@@ -1,61 +1,62 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
+const { DateTime } = require('luxon')
+const CleanCSS = require('clean-css')
+const fs = require('fs')
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
 
 module.exports = function (eleventyConfig) {
   // https://www.11ty.dev/docs/data-deep-merge/
-  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true)
 
   // Alias `layout: post` to `layout: layouts/post.njk`
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
 
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
-    );
-  });
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy')
+  })
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
-  });
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd')
+  })
 
   // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
+  eleventyConfig.addFilter('head', (array, n) => {
     if (n < 0) {
-      return array.slice(n);
+      return array.slice(n)
     }
 
-    return array.slice(0, n);
-  });
+    return array.slice(0, n)
+  })
 
   // Return the smallest number argument
-  eleventyConfig.addFilter("min", (...numbers) => {
-    return Math.min.apply(null, numbers);
-  });
+  eleventyConfig.addFilter('min', (...numbers) => {
+    return Math.min.apply(null, numbers)
+  })
 
-  eleventyConfig.addFilter("filterTagList", (tags) => {
+  eleventyConfig.addFilter('filterTagList', (tags) => {
     // should match the list in tags.njk
-    return (tags || []).filter(
-      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
-    );
-  });
+    return (tags || []).filter((tag) => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1)
+  })
+
+  eleventyConfig.addFilter('cssmin', function (code) {
+    return new CleanCSS({}).minify(code).styles
+  })
 
   // Create an array of all tags
-  eleventyConfig.addCollection("tagList", function (collection) {
-    let tagSet = new Set();
+  eleventyConfig.addCollection('tagList', function (collection) {
+    let tagSet = new Set()
     collection.getAll().forEach((item) => {
-      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
-    });
+      ;(item.data.tags || []).forEach((tag) => tagSet.add(tag))
+    })
 
-    return [...tagSet];
-  });
+    return [...tagSet]
+  })
 
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('favicon.ico')
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
@@ -64,33 +65,33 @@ module.exports = function (eleventyConfig) {
     linkify: true,
   }).use(markdownItAnchor, {
     permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#",
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#',
+  })
+  eleventyConfig.setLibrary('md', markdownLibrary)
 
   // Override Browsersync defaults (used only with --serve)
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("_site/404.html");
+        const content_404 = fs.readFileSync('_site/404.html')
 
-        browserSync.addMiddleware("*", (req, res) => {
+        browserSync.addMiddleware('*', (req, res) => {
           // Provides the 404 content without redirect.
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-          res.write(content_404);
-          res.end();
-        });
+          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' })
+          res.write(content_404)
+          res.end()
+        })
       },
     },
     ui: false,
     ghostMode: false,
-  });
+  })
 
   return {
     // Control which files Eleventy will process
     // e.g.: *.md, *.njk, *.html, *.liquid
-    templateFormats: ["md", "njk", "html", "liquid"],
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
 
     // -----------------------------------------------------------------
     // If your site deploys to a subdirectory, change `pathPrefix`.
@@ -103,24 +104,24 @@ module.exports = function (eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "/",
+    pathPrefix: '/',
     // -----------------------------------------------------------------
 
     // Pre-process *.md files with: (default: `liquid`)
-    markdownTemplateEngine: "njk",
+    markdownTemplateEngine: 'njk',
 
     // Pre-process *.html files with: (default: `liquid`)
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: 'njk',
 
     // Opt-out of pre-processing global data JSON files: (default: `liquid`)
     dataTemplateEngine: false,
 
     // These are all optional (defaults are shown):
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site",
+      input: '.',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
     },
-  };
-};
+  }
+}
